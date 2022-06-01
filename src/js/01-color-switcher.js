5bb0ec38bd59007b43d35b1f31b60a3b33dea40c
Taras Bondar <strelezzzz@gmail.com>
2022-06-02 00:13:44 +0300

    Update 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,30 +9,31 @@ const refs = {
   startButton: document.querySelector('button[data-start]'),
   stopButton: document.querySelector('button[data-stop]'),
 };
-
+refs.stopButton.disabled = true;
 let intervalId = null; // правило хорошошо тону :)
 let isActive = false; //присвоюємо початковий статус, який нижче будем змінювати, коли натиснута кнопка Старт
-
+// ====================================
 refs.startButton.addEventListener('click', () => {
   //вішаємо слухача на кнопку "Старт"
   if (isActive) {
-    //Перевіряємо статус кнопки і якщо true
-    return console.log('"START" is already pressed'); //зразу ж виходимо + пишемо в консоль
+    //Перевіряємо статус кнопки і якщо true виходимо
+    return;
   }
   isActive = true; //міняємо статус кнопки
+  console.log('START');
   intervalId = setInterval(() => {
-    //запускаємо інтервал, створюємо intervalId
-    console.log('START');
-    // console.log(getRandomHexColor());
+    //запускаємо інтервал та створюємо intervalId
     refs.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
+  refs.startButton.disabled = true; // вимикаємо вмикаємо відповідні кнопки
+  refs.stopButton.disabled = false;
 });
-
+// ================================================================
 refs.stopButton.addEventListener('click', () => {
   //вішаємо слухача на кнопку "Стоп"
   clearInterval(intervalId); //зупиняємо інтервал по (intervalId)
   isActive = false; //змінюємо статус кнопки "Старт"
+  refs.startButton.disabled = false;
+  refs.stopButton.disabled = true;
   console.log('STOP');
 });
-
-// console.log(intervalId);
